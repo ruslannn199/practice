1f4b28c4558f303101c77b00121cb90c2daa58ce
ruslannn199 <ruslannn199@gmail.com>
2025-04-08 21:32:08 +0300

    fix: body parser

--- src/server/app.ts
+++ src/server/app.ts
@@ -2,11 +2,13 @@ import express from "express";
 import { router } from "./router";
 import { pool } from "./db";
 import { seedData } from "./seed";
-
+import bodyParser from "body-parser";
+import cors from "cors";
 const app = express();
 
-app.use(express.json());
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({ origin: "*" }));
 app.use(router);
 
 const port = 8000;
